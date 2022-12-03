7c60fa2c2b68320fd24b17afe4a8d08eaa32b0cb
Michał Kowieski <michael.kowieski@gmail.com>
2022-12-03 12:00:42 +0100

    🥳 Day 3b solved

--- AoC-2022/3.ts
+++ AoC-2022/3.ts
@@ -1,16 +1,32 @@
-type Compartment = Array<string>;
+type Items = Array<string>;
 
-function makeCompartments(rawRucksacks: string): Array<Compartment> {
-  const middleIdx = rawRucksacks.length / 2;
-  const firstRawCompartment = rawRucksacks.slice(0, middleIdx);
-  const secondRawCompartment = rawRucksacks.slice(
-    middleIdx,
-    rawRucksacks.length,
-  );
-  return [firstRawCompartment.split(""), secondRawCompartment.split("")];
+// function makeCompartments(rawRucksack: string): Array<Items> {
+//   const middleIdx = rawRucksack.length / 2;
+//   const firstRawCompartment = rawRucksack.slice(0, middleIdx);
+//   const secondRawCompartment = rawRucksack.slice(
+//     middleIdx,
+//     rawRucksack.length,
+//   );
+//   return [firstRawCompartment.split(""), secondRawCompartment.split("")];
+// }
+
+type Group = Items[];
+
+function makeGroups(rawRucksacks: string[]): Group[] {
+  const groups = [];
+  for (let i = 0; (i + 2) < rawRucksacks.length; i += 3) {
+    const group: Group = [
+      rawRucksacks[i + 0].split(""),
+      rawRucksacks[i + 1].split(""),
+      rawRucksacks[i + 2].split(""),
+    ];
+    groups.push(group);
+  }
+
+  return groups;
 }
 
-function makeSharedItems(compartments: Compartment[]): Set<string> {
+function makeSharedItems(compartments: Items[]): Set<string> {
   if (compartments.length === 0) {
     return new Set();
   }
@@ -28,7 +44,7 @@ function makeSharedItems(compartments: Compartment[]): Set<string> {
   return sharedItems;
 }
 
-function findSharedItem(compartments: Compartment[]): string {
+function findSharedItem(compartments: Items[]): string {
   const sharedItems = makeSharedItems(compartments);
   if (sharedItems.size !== 1) {
     throw Error("Exist more than one item");
@@ -52,15 +68,15 @@ async function main() {
   const rawRucksacks = text.split("\n");
   rawRucksacks.pop(); // last row is empty
 
-  const compartmentsByIdx = rawRucksacks.map(makeCompartments);
-  const sharedItemByIdx = compartmentsByIdx.map(findSharedItem);
+  const groups = makeGroups(rawRucksacks);
+  const sharedItemByIdx = groups.map(findSharedItem);
   const priorityByIdx = sharedItemByIdx.map(makePrioritizeValue);
   const prioritiesSum = priorityByIdx.reduce((sum, value) => sum + value, 0);
 
   console.log({
     priorityByIdx: priorityByIdx.slice(0, 10),
     sharedItemByIdx: sharedItemByIdx.slice(0, 10),
-    prioritiesSum
+    prioritiesSum,
   });
 }
 
