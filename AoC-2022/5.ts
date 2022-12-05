5935949d07c3135418a5bec50efd646b1126527a
Michał Kowieski <michael.kowieski@gmail.com>
2022-12-05 18:55:19 +0100

    🥳 Day 5a solved

---
+++ AoC-2022/5.ts
@@ -0,0 +1,66 @@
+type Stack = string[];
+type State = Array<Stack>;
+
+function makeStack(rawStack: string): Stack {
+  return rawStack.split("").reverse();
+}
+
+type Move = {
+  quantity: number;
+  from: number;
+  to: number;
+};
+
+function makeMove(rawMove: string): Move {
+  const [quantityAsString, fromAsString, toAsString] = rawMove.split(" ");
+  return {
+    quantity: Number.parseInt(quantityAsString),
+    from: Number.parseInt(fromAsString),
+    to: Number.parseInt(toAsString),
+  };
+}
+
+function runMove(state: State, move: Move) {
+  for (const _ of Array(move.quantity)) {
+    runSingleOperation(state, move);
+  }
+}
+
+type Operation = Pick<Move, "from" | "to">;
+
+function runSingleOperation(state: State, operation: Operation) {
+  const fromStack = state[operation.from - 1];
+  const toStack = state[operation.to - 1];
+
+  const movedItem = fromStack.pop();
+  if (movedItem === undefined) {
+    throw new Error();
+  }
+
+  toStack.push(movedItem);
+}
+
+function makeOutput(state: State): string {
+  return state.flatMap((stack) => stack.at(-1) ?? []).join("");
+}
+
+async function main() {
+  const initStateText = await Deno.readTextFile("./inputs/5-init-state.txt");
+  const rawInitStateText = initStateText.split("\n");
+  rawInitStateText.pop();
+  const state: State = rawInitStateText.map(makeStack);
+
+  const movesText = await Deno.readTextFile("./inputs/5-moves.txt");
+  const rawMoves = movesText.split("\n");
+  rawMoves.pop();
+  const moves = rawMoves.map(makeMove);
+
+  for (const move of moves) {
+    runMove(state, move);
+  }
+
+  const output = makeOutput(state);
+  console.log(output)
+}
+
+main();
