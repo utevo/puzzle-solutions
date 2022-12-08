34a5893278bfc94200a3c1fa2a81104e01f9ce61
Michał Kowieski <michael.kowieski@gmail.com>
2022-12-08 15:03:05 +0100

    🥳 Day 8 solved

---
+++ AoC-2022/8.ts
@@ -0,0 +1,89 @@
+function isTreeVisible(
+  treeMap: number[][],
+  position: [y: number, x: number],
+): boolean {
+  const [thisY, thisX] = position;
+  const m = treeMap.length;
+  const n = treeMap[0].length;
+  const thisTreeHeight = treeMap[thisY][thisX];
+
+  function isVisibleFromTop(): boolean {
+    if (thisY === 0) {
+      return true;
+    }
+
+    for (let yi = 0; yi < thisY; yi++) {
+      if (treeMap[yi][thisX] >= thisTreeHeight) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  function isVisibleFromBot(): boolean {
+    if (thisY === m - 1) {
+      return true;
+    }
+
+    for (let yi = thisY + 1; yi < m; yi++) {
+      if (treeMap[yi][thisX] >= thisTreeHeight) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  function isVisibleFromLeft(): boolean {
+    if (thisX === 0) {
+      return true;
+    }
+
+    for (let xi = 0; xi < thisX; xi++) {
+      if (treeMap[thisY][xi] >= thisTreeHeight) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  function isVisibleFromRight(): boolean {
+    if (thisX === n - 1) {
+      return true;
+    }
+
+    for (let xi = thisX + 1; xi < n; xi++) {
+      if (treeMap[thisY][xi] >= thisTreeHeight) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  return isVisibleFromLeft() || isVisibleFromRight() || isVisibleFromTop() ||
+    isVisibleFromBot();
+}
+
+async function main() {
+  const text = await Deno.readTextFile("./inputs/8.txt");
+  const textRows = text.split("\n");
+  textRows.pop();
+
+  const treeMap = textRows.map((textRow) => textRow.split("").map(Number));
+  const m = treeMap.length;
+  const n = treeMap[0]?.length ?? 0;
+
+  let visibleTrees = 0;
+  for (let yi = 0; yi < m; yi++) {
+    for (let xi = 0; xi < n; xi++) {
+      visibleTrees += isTreeVisible(treeMap, [yi, xi]) ? 1 : 0;
+    }
+  }
+
+  console.log({ treeMap, m, n, visibleTrees });
+}
+
+await main();
