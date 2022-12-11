b6b29d15da4e8af0e7c95045041bf78fae59cdee
Michał Kowieski <michael.kowieski@gmail.com>
2022-12-11 17:13:24 +0100

    🥳 Day 11b solved

--- src/aoc_2022/11.kt
+++ src/aoc_2022/11.kt
@@ -2,10 +2,10 @@ package aoc_2022
 
 import java.io.File
 
-typealias Item = Int
+typealias Item = Long
 
 sealed class MonkeyOperationExpression
-data class IntMOV(val value: Int) : MonkeyOperationExpression()
+data class IntMOV(val value: Item) : MonkeyOperationExpression()
 object OldMOV : MonkeyOperationExpression()
 
 
@@ -13,7 +13,7 @@ enum class MonkeyOperationOperator {
     ADD, MULTIPLY
 }
 
-fun MonkeyOperationExpression.value(old: Int): Int = when (this) {
+fun MonkeyOperationExpression.value(old: Item): Item = when (this) {
     OldMOV -> old
     is IntMOV -> this.value
 }
@@ -22,7 +22,7 @@ fun MonkeyOperationExpression.value(old: Int): Int = when (this) {
 data class MonkeyOperation(
     val left: MonkeyOperationExpression, val operator: MonkeyOperationOperator, val right: MonkeyOperationExpression
 ) {
-    fun calc(old: Int): Int {
+    fun calc(old: Item): Item {
         val leftValue = left.value(old)
         val rightValue = right.value(old)
 
@@ -45,20 +45,22 @@ data class Monkey(
 
 data class MonkeyThrow(val item: Item, val toMonkeyId: Int)
 
-fun Monkey.toMonkeyId(item: Item): Int =
-    if (toNewValue(item) % test.divisibleBy == 0) ifTrueTrowTo else ifFalseTrowTo
 
-fun Monkey.toNewValue(item: Item): Item = operation.calc(item).floorDiv(3)
+fun Monkey.toMonkeyId(item: Item, newValModulo: Int): Int =
+    if ((toNewValue(item, newValModulo) % test.divisibleBy.toLong()) == 0L) ifTrueTrowTo else ifFalseTrowTo
 
-fun Monkey.turnThrows(): List<MonkeyThrow> = items.map { MonkeyThrow(toNewValue(it), toMonkeyId(it)) }
+fun Monkey.toNewValue(item: Item, newValModulo: Int): Item = operation.calc(item) % newValModulo
+
+fun Monkey.turnThrows(newValModulo: Int): List<MonkeyThrow> =
+    items.map { MonkeyThrow(toNewValue(it, newValModulo), toMonkeyId(it, newValModulo)) }
 
 fun List<String>.toMonkey(): Monkey {
-    fun String.toItems(): List<Item> = this.split(", ").map { it.toInt() }
+    fun String.toItems(): List<Item> = this.split(", ").map { it.toLong() }
 
     fun String.toOperation(): MonkeyOperation {
         fun String.toExpression(): MonkeyOperationExpression = when (this) {
             "old" -> OldMOV
-            else -> IntMOV(this.toInt())
+            else -> IntMOV(this.toLong())
         }
 
         fun String.toOperator(): MonkeyOperationOperator = when (this) {
@@ -83,8 +85,8 @@ fun List<String>.toMonkey(): Monkey {
     )
 }
 
-fun monkeyBusiness(inspectionsCountByMonkeyId: List<Int>): Int =
-    inspectionsCountByMonkeyId.sorted().takeLast(2).reduce { acc, i -> acc * i }
+fun monkeyBusiness(inspectionsCountByMonkeyId: List<Int>): Long =
+    inspectionsCountByMonkeyId.sorted().takeLast(2).map { it.toLong() }.reduce { acc, i -> acc * i }
 
 fun main() {
     val lines = File("src/aoc_2022/inputs/11.txt").readLines()
@@ -93,9 +95,11 @@ fun main() {
 
     val inspectionsCountByMonkeyId = MutableList(monkeys.size) { 0 }
 
-    for (roundIdx in 1..20) {
+    val newValModulo = monkeys.map { it.test.divisibleBy }.reduce { acc, i -> acc * i }
+
+    for (roundIdx in 1..10000) {
         for (tourIdx in 0 until monkeys.size) {
-            val turnThrows = monkeys[tourIdx].turnThrows()
+            val turnThrows = monkeys[tourIdx].turnThrows(newValModulo)
             inspectionsCountByMonkeyId[tourIdx] += turnThrows.size
 
             for ((item, toMonkeyIdx) in turnThrows) {
@@ -105,7 +109,6 @@ fun main() {
         }
     }
 
-    println(inspectionsCountByMonkeyId)
     println(
         "monkeyBusiness: ${
             monkeyBusiness(inspectionsCountByMonkeyId)
