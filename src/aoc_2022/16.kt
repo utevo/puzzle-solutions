466a3d39ff95a3fe784e003635fcd4e7584eae9d
Michał Kowieski <michael.kowieski@gmail.com>
2022-12-17 17:27:41 +0100

    🥳 Day 16b solved

--- src/aoc_2022/16.kt
+++ src/aoc_2022/16.kt
@@ -62,12 +62,11 @@ class Rooms(roomList: List<Room>) {
 data class State(
     val rooms: Rooms,
     val currRoom: Room,
-    val leftMinutes: Int = 30,
-    val openedRooms: Set<Room> = setOf(),
+    val leftMinutes: Int = 26,
     val finalPressure: Int = 0,
 ) {
 
-    fun shouldTryGoingAndOpeningValue(toRoom: Room): Boolean {
+    fun shouldTryGoingAndOpeningValue(toRoom: Room, openedRooms: Set<Room>): Boolean {
         val thisIsPossible = toRoom !in openedRooms
         val thisMakeSense = toRoom.flowRate != 0
         val weHaveTimeForThis = rooms.distance(currRoom.name, toRoom.name) + 2 <= leftMinutes
@@ -81,11 +80,33 @@ data class State(
         val gainedPressure = (leftMinutes - neededTime) * toRoom.flowRate
         return this.copy(
             currRoom = toRoom,
-            openedRooms = openedRooms.plus(toRoom),
             finalPressure = finalPressure + gainedPressure,
             leftMinutes = leftMinutes - neededTime
         )
     }
+}
+
+fun limitOfImprovement(yourState: State, elephantState: State, openedRooms: Set<Room>): Int {
+    var limitOfImprovement = 0
+    val notOpenedRoomsSorted =
+        (yourState.rooms.usefulRooms - openedRooms).sortedBy { it.flowRate }.toMutableList()
+
+    val yourNearestRoom = notOpenedRoomsSorted.minOf { yourState.rooms.distance(yourState.currRoom.name, it.name) }
+    val elephantNearestRoom =
+        notOpenedRoomsSorted.minOf { yourState.rooms.distance(elephantState.currRoom.name, it.name) }
+    var yourLeftMinutes = yourState.leftMinutes - yourNearestRoom + 1
+    var elephantLeftMinutes = elephantState.leftMinutes - elephantNearestRoom + 1
+
+    while ((yourLeftMinutes >= 2 || elephantLeftMinutes >= 2) && notOpenedRoomsSorted.isNotEmpty()) {
+        if (yourLeftMinutes > elephantLeftMinutes) {
+            yourLeftMinutes -= 2
+            limitOfImprovement += yourLeftMinutes * notOpenedRoomsSorted.removeLast().flowRate
+        } else {
+            elephantLeftMinutes -= 2
+            limitOfImprovement += elephantLeftMinutes * notOpenedRoomsSorted.removeLast().flowRate
+        }
+    }
+    return limitOfImprovement
 }
 
 fun main() {
@@ -94,20 +115,44 @@ fun main() {
     val rooms = Rooms(roomList)
 
     var maxPressure = 0
-
-    fun backtrack(state: State) {
-        if (maxPressure < state.finalPressure) {
-            maxPressure = state.finalPressure
+    fun backtrack(yourState: State, elephantState: State, openedRooms: MutableSet<Room>) {
+        val currFinalPressure = yourState.finalPressure + elephantState.finalPressure
+        if (maxPressure < currFinalPressure) {
+            maxPressure = currFinalPressure
+            println("---")
+            println(currFinalPressure)
+        } else {
+            if (currFinalPressure + limitOfImprovement(yourState, elephantState, openedRooms) < maxPressure) {
+                return
+            }
         }
-        val roomsToChock = rooms.usefulRooms - state.openedRooms
-        for (room in roomsToChock) {
-            if (state.shouldTryGoingAndOpeningValue(room)) {
-                backtrack(state.afterGoingAndOpeningValue(toRoom = room))
+
+        for (room in (rooms.usefulRooms - openedRooms)) {
+            if (yourState.shouldTryGoingAndOpeningValue(room, openedRooms)) {
+                openedRooms.add(room)
+                backtrack(yourState.afterGoingAndOpeningValue(toRoom = room), elephantState, openedRooms)
+                openedRooms.remove(room)
+            }
+
+            if (elephantState.shouldTryGoingAndOpeningValue(room, openedRooms)) {
+                openedRooms.add(room)
+                backtrack(yourState, elephantState.afterGoingAndOpeningValue(toRoom = room), openedRooms)
+                openedRooms.remove(room)
             }
         }
     }
 
-    backtrack(State(rooms = rooms, currRoom = rooms.roomByName.getValue(START_ROOM_NAME)))
+    backtrack(
+        yourState = State(
+            rooms = rooms,
+            currRoom = rooms.roomByName.getValue(START_ROOM_NAME),
+        ),
+        elephantState = State(
+            rooms = rooms,
+            currRoom = rooms.roomByName.getValue(START_ROOM_NAME),
+        ),
+        mutableSetOf()
+    )
 
-    println(maxPressure)
+    println("maxPressure $maxPressure")
 }
